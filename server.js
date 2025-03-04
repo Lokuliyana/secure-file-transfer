c864a67a6a968a7e09c2c0154120c92fe86a7160
Lokuliyana <lokuliyanacp@gmail.com>
2025-03-04 10:41:40 +0530

    Initial project setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json()); // Middleware to parse JSON
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
